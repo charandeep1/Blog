d22afbb413c61e3f664d85b97332c560646b6b95
charan <satellyc@gmail.com>
2022-04-25 20:16:01 -0500

    scroll function

---
+++ main.js
@@ -0,0 +1,34 @@
+// Grad Elements
+
+const selectElement = selector => {
+    const element = document.querySelector(selector)
+    if (element) return element;
+    throw new Error('Something went wrong, make sure that the $(selector) exists or is typed correctly');
+
+};
+
+// Nav styles on scroll
+
+const scrollHeader = () => {
+    const headerElement = selectElement('#header');
+    if (this.scrollY >= 15) {
+        headerElement.classList.add('activated');
+    }
+    else {
+        headerElement.classList.remove('activated');
+    }
+};
+
+window.addEventListener('scroll', scrollHeader);
+
+//Open menu and search pop up
+
+const menuToggleIcon = selectElement('#menu-toggle-icon');
+
+const toggleMenu = () => {
+    const mobileMenu = selectElement('#menu');
+    mobileMenu.classList.toggle('activated');
+    menuToggleIcon.classList.toggle('activated');
+};
+
+menuToggleIcon.addEventListener('click', toggleMenu);
